4b239f569e84270dac7c8dcdeac17f472ac5fb74
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-02-10 00:09:56 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-center py-7 text-500 text-orange-300'>
+        React Typescript Projects
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+import 'primereact/resources/themes/lara-light-cyan/theme.css';
+import '/node_modules/primeflex/primeflex.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
