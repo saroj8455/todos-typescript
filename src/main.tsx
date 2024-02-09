f465bc2dd8d540e5aeb005618c360d082c147081
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-02-10 00:12:04 +0530

    font added

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 
 import 'primereact/resources/themes/lara-light-cyan/theme.css';
 import '/node_modules/primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
